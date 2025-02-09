de527b00718a7dfd1f28aebfbf6743ae92bfe96d
Deepu Mohan Puthrote <186013+WarFox@users.noreply.github.com>
2025-02-09 19:07:54 +0000

    Setup openapi generator for client and server interfaces

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.0.0"
 }
 
 group = "io.github.warfox"
@@ -21,6 +22,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jooq")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
@@ -46,4 +50,30 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/src/main/resources/openapi.yaml")
+    outputDir.set("${layout.buildDirectory.asFile.get()}/generated")
+    apiPackage.set("io.github.warfox.app.api")
+    modelPackage.set("io.github.warfox.app.model")
+    configOptions.set(
+        mapOf(
+            "useTags" to "true",
+            "useSpringBoot3" to "true"
+        )
+    )
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("${layout.buildDirectory.asFile.get()}/generated/src/main/java")
+        }
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("openApiGenerate")
 }
