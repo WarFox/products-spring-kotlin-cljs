2af77d24f4270c2185bedd6579b711c9107f7466
Deepu Mohan Puthrote <186013+WarFox@users.noreply.github.com>
2025-03-17 22:08:52 +0000

    implement product controller api

--- src/main/kotlin/io/github/warfox/app/controller/ProductController.kt
+++ src/main/kotlin/io/github/warfox/app/controller/ProductController.kt
@@ -1,25 +1,22 @@
 package io.github.warfox.app.controller
 
+import io.github.warfox.app.api.ProductControllerApi
 import io.github.warfox.app.domain.toProductV1
 import io.github.warfox.app.model.ProductV1
 import io.github.warfox.app.service.ProductService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
 import java.util.UUID
 
 @Controller
-class ProductController(val service: ProductService) {
-    @RequestMapping("/products")
-    fun listProducts(): ResponseEntity<List<ProductV1>> {
+class ProductController(val service: ProductService): ProductControllerApi{
+    override fun listProducts(): ResponseEntity<List<ProductV1>> {
         return service.listProducts().let {
             ResponseEntity.ok(it?.map { it.toProductV1() })
         }
     }
 
-    @RequestMapping("/products/{id}")
-    fun getProduct(id: UUID): ResponseEntity<ProductV1> {
+    override fun getProduct(id: UUID): ResponseEntity<ProductV1> {
         return ResponseEntity.ok(service.getProduct(id)?.toProductV1())
     }
-
 }
